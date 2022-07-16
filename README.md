dbfc21607fd754340bc507e4f00155c7f37548d9
Patato <pathonbusiness@gmail.com>
2022-07-16 15:35:05 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This is a NextJS Template
+
+## Getting Started
+
+Use this template and clone it
+
+`yarn` or `npm install`
+
+### Development
+
+```
+yarn dev
+```
+
+### Production
+
+`yarn build`
+
+`yarn start`
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="fullflex">
+      <h1 className="text-6xl text-blue-500">NextJS + TailwindCSS🍃</h1>
+      <h1 className="text-5xl text-blue-400">Template</h1>
+
+    </div>
+  )
+}
+
+export default Home
